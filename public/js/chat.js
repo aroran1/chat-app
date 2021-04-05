00c95a2ccb1040c3704dd80be7bc7b14d83e9668
aroran1 <nidhigulati2001@gmail.com>
2021-04-05 18:09:44 +0100

    updated user interface for buttons interactions

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,34 +1,47 @@
 const socket = io();
+const $messageForm = document.querySelector('#message-form');
+const $messageFormInput = document.querySelector('input');
+const $messageFormButton = document.querySelector('button');
+const $sendLocationButton = document.querySelector('#send-location');
 
 // receiving message
 socket.on('message', (message) => {
   console.log(message);
 })
 
-document.querySelector('#message-form').addEventListener('submit', (e) => {
+$messageForm.addEventListener('submit', (e) => {
   e.preventDefault();
+
+  // disable send button once a message is sent
+  $messageFormButton.setAttribute('disabled', 'disabled')  
 
   // const message = document.querySelector('input').value;
   const message = e.target.elements.message.value
+
   // emitting message
   // socket.emit('sendMessage', message, (ackMsg) => {
-  //   // logging acknowledge message with returned acknowledge message ackMsg = 'Delivered!'
-  //   console.log('This message is delivered!', ackMsg);
-  // })
-
   socket.emit('sendMessage', message, (error) => {
+    // enable send button once its recieved / acknowledged by serve
+    $messageFormButton.removeAttribute('disabled')
+    $messageFormInput.value = ''
+    $messageFormInput.focus()
+
+
     // logging acknowledge message with returned acknowledge message ackMsg = 'Delivered!'
     if (error) {
        return console.log(error);
     }
+    // console.log('This message is delivered!', ackMsg);
     console.log('Message delivered!');
   })
 });
 
-document.querySelector('#send-location').addEventListener('click', () => {
+$sendLocationButton.addEventListener('click', () => {
   if(!navigator.geolocation) {
     return alert('Geolocation is not supported by their browser!')
   }
+  // disable send location button
+  $sendLocationButton.setAttribute('disabled', 'disabled');
 
   navigator.geolocation.getCurrentPosition((position) => {
     // console.log(position);
@@ -36,6 +49,8 @@ document.querySelector('#send-location').addEventListener('click', () => {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     }, () => {
+      // enable send location button
+      $sendLocationButton.removeAttribute('disabled');
       console.log('Location shared!');
     });
   })
